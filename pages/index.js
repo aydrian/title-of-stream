c8ae62ae3e2764671e8eca3387be632c2a3ae516
Aydrian Howard <aydrian@gmail.com>
2021-03-20 10:18:10 -0400

    Updated index page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>[title of stream]</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="[title of stream]" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Join <a href="https://twitch.tv/itsaydrian">Aydrian</a>,{" "}
+          <a href="https://twitch.tv/chloecondon">Chloe</a>, and{" "}
+          <a href="https://twitch.tv/LaRenaiocco">LaRena</a> Monday nights at
+          3pm Pacific/6pm Eastern.
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
